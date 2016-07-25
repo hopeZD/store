200151333f3e8d008d985b0df4539098c768a966
hope <zhengzuodi@gmail.com>
2016-07-25 16:05:05 +0800

    add register

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,9 +2,13 @@
 
 use App\Entity\Member;
 
-Route::get('/', function() {
-  return view('login');
-});
+    Route::get('/', function() {
+      return view('login');
+    });
+
+    Route::get('/', function() {
+        return view('register');
+    });
 
 
 Route::any('/service/validate_code/create', 'Service\ValidateCodeController@create');

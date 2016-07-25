375465ef16d0e8eb8ddefb292bfa923e66a9d520
hope <zhengzuodi@gmail.com>
2016-07-25 15:50:41 +0800

    add validateCode jquery

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -37,5 +37,9 @@
 @endsection
 
 @section('my-js')
-
+    <script type="text/javascript">
+        $(this).attr('.bk_validate_code').click(function () {
+            $(this).attr('src', '/service/validate_code/create?random=' + Math.random());
+        });
+    </script>
 @endsection()

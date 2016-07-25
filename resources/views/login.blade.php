cdbe04879da23499eeef3ae0106db54717cf3f06
hope <zhengzuodi@gmail.com>
2016-07-25 15:59:20 +0800

    add validateCode agian

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -38,8 +38,10 @@
 
 @section('my-js')
     <script type="text/javascript">
-        $(this).attr('.bk_validate_code').click(function () {
+        $('.bk_validate_code').click(function () {
             $(this).attr('src', '/service/validate_code/create?random=' + Math.random());
         });
+
     </script>
+    
 @endsection()
